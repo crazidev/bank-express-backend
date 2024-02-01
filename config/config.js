023496282557d4b1b8074ec080a6929d5b326687
Honesty Nyong <crazibeatdeveloper@gmail.com>
2024-02-02 00:55:31 +0100

    #

--- config/config.js
+++ config/config.js
@@ -6,7 +6,11 @@ module.exports = {
     storage: process.env.SQLITE || "sqlite.db",
   },
   production: {
-    dialect: "sqlite",
-    storage: process.env.SQLITE || "sqlite.db",
+    dialect: "mysql",
+    database: process.env.MYSQL_DB_NAME,
+    username: process.env.MYSQL_DB_USERNAME,
+    password: process.env.MYSQL_DB_PASSWORD,
+    host: process.env.MYSQL_DB_HOST,
+    port: parseInt(process.env.MYSQL_DB_PORT),
   },
 };
