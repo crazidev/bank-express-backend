a3c6582fed2d2be85c51e02fc543d235a2f04cfd
Honesty Nyong <crazibeatdeveloper@gmail.com>
2024-04-11 04:55:16 +0100

    #

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: "gfcedit_server_1",
+      name: "bank_server_1",
       script: "build/index.js",
       watch: true,
       env: {
         NODE_ENV: "production",
         PORT: 4001,
-        name: "gfcedit_server_1",
+        name: "bank_server_1",
       },
       env_development: {
         NODE_ENV: "development",
@@ -15,13 +15,13 @@ module.exports = {
     },
     // ,
     // {
-    //   name: "hybank_server_2",
+    //   name: "bank_server_2",
     //   script: "build/index.js",
     //   watch: true,
     //   env: {
     //     NODE_ENV: "production",
     //     PORT: 3002,
-    //     name: "hybank_server_2",
+    //     name: "bank_server_2",
     //   },
     //   env_development: {
     //     NODE_ENV: "development",
